d570bb06e8bed041630ce188f598cbeb104ad4e8
IsaRK <isabelle.riverain@gmail.com>
2021-02-17 11:30:32 +0100

    searching for more info in context

--- questionplease-api/ReadOneUser.cs
+++ questionplease-api/ReadOneUser.cs
@@ -37,10 +37,52 @@ namespace questionplease_api
             var identityName = claimsPrincipal.Identity.Name;
             var identityString = claimsPrincipal.Identity.ToString();
             var NameIdentifierId = claimsPrincipal.GetNameIdentifierId();
-            var currentUserName = ClaimsPrincipal.Current.Identity.Name;
-            var otherIdentity = ClaimsPrincipal.Current.Identity.ToString();
-            var currentHomeObjectValue = ClaimsPrincipal.Current.GetHomeObjectId();
-            var currentMsalAccountId = ClaimsPrincipal.Current.GetMsalAccountId();
+            //var currentUserName = ClaimsPrincipal.Current.Identity.Name;
+            //var otherIdentity = ClaimsPrincipal.Current.Identity.ToString();
+            //var currentHomeObjectValue = ClaimsPrincipal.Current.GetHomeObjectId();
+            //var currentMsalAccountId = ClaimsPrincipal.Current.GetMsalAccountId();
+
+            if (req.Headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-NAME", out var principalName))
+            {
+                log.LogInformation($"principalName Id: {principalName}");
+            }
+            else
+            {
+                log.LogInformation($"No principalName");
+            }
+
+            if (req.Headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-ID", out var principalId))
+            {
+                log.LogInformation($"principalId Id: {principalId}");
+            }
+            else
+            {
+                log.LogInformation($"No principalId");
+            }
+
+            if (ClaimsPrincipal.Current == null)
+            {
+                log.LogInformation($"ClaimsPrincipal.Current is null");
+            }
+            else if (ClaimsPrincipal.Current.Identity == null)
+            {
+                log.LogInformation($"ClaimsPrincipal.Current.Identity is null");
+            }
+            else
+            {
+                log.LogInformation($"ClaimsPrincipal.Current.Identity is {ClaimsPrincipal.Current.Identity.ToString()}");
+            }
+
+            var userReq = req.HttpContext.User;
+            if (userReq == null)
+            {
+                log.LogInformation($"User from context is null");
+            }
+            else
+            {
+                log.LogInformation($"User from context: {userReq.GetDisplayName()}");
+            }
+
 
             log.LogInformation($"Searching for: {searchValue}");
             log.LogInformation($"Home Object ID: {homeObjectValue}");
@@ -48,10 +90,10 @@ namespace questionplease_api
             log.LogInformation($"IdentityName: {identityName}");
             log.LogInformation($"Name Identifier Id: {NameIdentifierId}");
             log.LogInformation($"IdentityString Id: {identityString}");
-            log.LogInformation($"currentUserName Id: {currentUserName}");
-            log.LogInformation($"otherIdentity Id: {otherIdentity}");
-            log.LogInformation($"currentHomeObjectValue Id: {currentHomeObjectValue}");
-            log.LogInformation($"currentMsalAccountId Id: {currentMsalAccountId}");
+            //log.LogInformation($"currentUserName Id: {currentUserName}");
+            //log.LogInformation($"otherIdentity Id: {otherIdentity}");
+            //log.LogInformation($"currentHomeObjectValue Id: {currentHomeObjectValue}");
+            //log.LogInformation($"currentMsalAccountId Id: {currentMsalAccountId}");
             string name = req.Query["name"];
 
             var option = new FeedOptions { EnableCrossPartitionQuery = true };
